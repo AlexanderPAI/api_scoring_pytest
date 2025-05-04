956e33bea78aaaa1626c81c63e3ccbe115e95c88
AlexanderPAI <lawlikeart@yandex.ru>
2025-05-04 20:05:44 +0300

    Test OnlineScoreRequest

--- tests/unit/requests/fixtures.py
+++ tests/unit/requests/fixtures.py
@@ -72,3 +72,35 @@ clients_interests_request_negative_fixtures = [
         id="NEGATIVE MethodRequest: No ClientIDs Error",
     ),
 ]
+
+
+online_score_request_positive_fixtures = [
+    pytest.param(
+        {"phone": "79175002040", "email": "stupnikov@otus.ru"},
+        id="POSITIVE OnlineScoreRequest: Success - Phone + Email",
+    ),
+    pytest.param(
+        {"first_name": "Станислав", "last_name": "Ступников"},
+        id="POSITIVE OnlineScoreRequest: Success - First_name + Last_name",
+    ),
+    pytest.param(
+        {"birthday": "01.01.2000", "gender": 1},
+        id="POSITIVE OnlineScoreRequest: Success - BirthDay + Gender",
+    ),
+]
+
+
+online_score_request_negative_fixtures = [
+    pytest.param(
+        {"phone": "79175002040", "first_name": "Станислав", "birthday": "01.01.2000"},
+        '"online_score" request must have at least one pair with non-empty values: phone - email, '
+        "first_name - last_name, birthday - gender.",
+        id="NEGATIVE OnlineScoreRequest: No Email, No Last_name, No Gender Error",
+    ),
+    pytest.param(
+        {"email": "stupnikov@otus.ru", "last_name": "Ступников", "gender": 1},
+        '"online_score" request must have at least one pair with non-empty values: phone - email, '
+        "first_name - last_name, birthday - gender.",
+        id="NEGATIVE OnlineScoreRequest: No Phone, No First_name, No BirthDay Error",
+    ),
+]
--- tests/unit/requests/test_requests.py
+++ tests/unit/requests/test_requests.py
@@ -1,11 +1,13 @@
 import pytest
 
-from src.api import ClientsInterestsRequest, MethodRequest
+from src.api import ClientsInterestsRequest, MethodRequest, OnlineScoreRequest
 from tests.unit.requests.fixtures import (
     clients_interests_request_negative_fixtures,
     clients_interests_request_positive_fixtures,
     method_request_negative_fixtures,
     method_request_positive_fixtures,
+    online_score_request_negative_fixtures,
+    online_score_request_positive_fixtures,
 )
 
 
@@ -32,4 +34,18 @@ def test_clients_interests_request_positive(request_body):
 def test_clients_interests_request_negative(request_body, expected):
     with pytest.raises(ValueError) as e:
         ClientsInterestsRequest(request_body)
+    assert str(e.value) == expected
+
+
+@pytest.mark.parametrize("request_body", online_score_request_positive_fixtures)
+def test_online_score_request_positive(request_body):
+    OnlineScoreRequest(request_body)
+
+
+@pytest.mark.parametrize(
+    "request_body, expected", online_score_request_negative_fixtures
+)
+def test_online_score_request_negative(request_body, expected):
+    with pytest.raises(ValueError) as e:
+        OnlineScoreRequest(request_body)
     assert str(e.value) == expected
