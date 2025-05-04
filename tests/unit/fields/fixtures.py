5ec87de985cac1949a0c94f9c4a6714b2935ae10
AlexanderPAI <lawlikeart@yandex.ru>
2025-05-04 17:12:06 +0300

    Refactoring field tests

--- tests/unit/fields/fixtures.py
+++ tests/unit/fields/fixtures.py
@@ -1,113 +1,40 @@
 import pytest
 
-# fields
+from src.api import ArgumentsField, CharField, Field
 
-# params for field positive validate
-field_positive_validate = [
+fields_positive_fixtures = [
     pytest.param(
+        Field,
         {"required": True, "nullable": True},
         "test_value",
         id='Field: required=True, nullable=True, value="test_value"',
     ),
     pytest.param(
+        Field,
         {"required": False, "nullable": True},
         "test_value",
         id='Field: required=False, nullable=True, value="test_value"',
     ),
     pytest.param(
+        Field,
         {"required": True, "nullable": False},
         "test_value",
         id='Field: required=True, nullable=False, value="test_value"',
     ),
     pytest.param(
+        Field,
         {"required": False, "nullable": False},
         "test_value",
         id='Field: required=False, nullable=False, value="test_value"',
     ),
-]
-
-# params for field negative validate
-field_negative_validate = [
     pytest.param(
-        {"required": True, "nullable": False},
-        None,
-        "{field_name} is required",
-        id="Field: required=True, nullable=False, value=None",
-    ),
-    pytest.param(
-        {"required": True, "nullable": False},
-        "",
-        "{field_name} isn't nullable",
-        id='Field: required=True, nullable=False, value=""',
-    ),
-    pytest.param(
-        {"required": True, "nullable": False},
-        (),
-        "{field_name} isn't nullable",
-        id="Field: required=True, nullable=False, value=()",
-    ),
-    pytest.param(
-        {"required": True, "nullable": False},
-        {},
-        "{field_name} isn't nullable",
-        id="Field: required=True, nullable=False, value={}",
-    ),
-    pytest.param(
-        {"required": True, "nullable": False},
-        [],
-        "{field_name} isn't nullable",
-        id="Field: required=True, nullable=False, value=[]",
-    ),
-]
-
-
-# params for char_field positive validate
-char_field_positive_validate = [
-    pytest.param(
+        CharField,
         {"required": True, "nullable": False},
         "char_test_value",
         id="CharField: value is str",
     ),
-]
-
-
-# params for char_field negative validate
-char_field_negative_validate = [
     pytest.param(
-        {"required": True, "nullable": False},
-        1,
-        "{field_name} must be a string",
-        id="CharField: value is int",
-    ),
-    pytest.param(
-        {"required": True, "nullable": False},
-        1.0,
-        "{field_name} must be a string",
-        id="CharField: value is float",
-    ),
-    pytest.param(
-        {"required": True, "nullable": False},
-        True,
-        "{field_name} must be a string",
-        id="CharField: value is bool",
-    ),
-    pytest.param(
-        {"required": True, "nullable": False},
-        ["one", "two"],
-        "{field_name} must be a string",
-        id="CharField: value is list",
-    ),
-    pytest.param(
-        {"required": True, "nullable": False},
-        {"key1": "value1", "key2": "value2"},
-        "{field_name} must be a string",
-        id="CharField: value is dict",
-    ),
-]
-
-
-arguments_field_positive_validate = [
-    pytest.param(
+        ArgumentsField,
         {"required": True, "nullable": False},
         {"key1": "value1", "key2": "value2"},
         id="ArgumentsField: value is dict",
@@ -115,32 +42,107 @@ arguments_field_positive_validate = [
 ]
 
 
-arguments_field_negative_validate = [
+fields_negative_fixtures = [
     pytest.param(
+        Field,
+        {"required": True, "nullable": False},
+        None,
+        "{field_name} is required",
+        id="Field: required=True, nullable=False, value=None",
+    ),
+    pytest.param(
+        Field,
+        {"required": True, "nullable": False},
+        "",
+        "{field_name} isn't nullable",
+        id='Field: required=True, nullable=False, value=""',
+    ),
+    pytest.param(
+        Field,
+        {"required": True, "nullable": False},
+        (),
+        "{field_name} isn't nullable",
+        id="Field: required=True, nullable=False, value=()",
+    ),
+    pytest.param(
+        Field,
+        {"required": True, "nullable": False},
+        {},
+        "{field_name} isn't nullable",
+        id="Field: required=True, nullable=False, value={}",
+    ),
+    pytest.param(
+        Field,
+        {"required": True, "nullable": False},
+        [],
+        "{field_name} isn't nullable",
+        id="Field: required=True, nullable=False, value=[]",
+    ),
+    pytest.param(
+        CharField,
+        {"required": True, "nullable": False},
+        1,
+        "{field_name} must be a string",
+        id="CharField: value is int",
+    ),
+    pytest.param(
+        CharField,
+        {"required": True, "nullable": False},
+        1.0,
+        "{field_name} must be a string",
+        id="CharField: value is float",
+    ),
+    pytest.param(
+        CharField,
+        {"required": True, "nullable": False},
+        True,
+        "{field_name} must be a string",
+        id="CharField: value is bool",
+    ),
+    pytest.param(
+        CharField,
+        {"required": True, "nullable": False},
+        ["one", "two"],
+        "{field_name} must be a string",
+        id="CharField: value is list",
+    ),
+    pytest.param(
+        CharField,
+        {"required": True, "nullable": False},
+        {"key1": "value1", "key2": "value2"},
+        "{field_name} must be a string",
+        id="CharField: value is dict",
+    ),
+    pytest.param(
+        ArgumentsField,
         {"required": True, "nullable": False},
         "test_value",
         "{field_name} must be a dictionary",
         id="ArgumentsField: value is str",
     ),
     pytest.param(
+        ArgumentsField,
         {"required": True, "nullable": False},
         1,
         "{field_name} must be a dictionary",
         id="ArgumentsField: value is int",
     ),
     pytest.param(
+        ArgumentsField,
         {"required": True, "nullable": False},
         1.0,
         "{field_name} must be a dictionary",
         id="ArgumentsField: value is float",
     ),
     pytest.param(
+        ArgumentsField,
         {"required": True, "nullable": False},
         True,
         "{field_name} must be a dictionary",
         id="ArgumentsField: value is bool",
     ),
     pytest.param(
+        ArgumentsField,
         {"required": True, "nullable": False},
         ["one", "two"],
         "{field_name} must be a dictionary",
--- tests/unit/fields/test_fields.py
+++ tests/unit/fields/test_fields.py
@@ -1,64 +1,26 @@
 import pytest
 
-from src.api import ArgumentsField, CharField, Field
-from tests.unit.fields.params import (
-    arguments_field_negative_validate,
-    arguments_field_positive_validate,
-    char_field_negative_validate,
-    char_field_positive_validate,
-    field_negative_validate,
-    field_positive_validate,
+from tests.unit.fields.fixtures import (
+    fields_negative_fixtures,
+    fields_positive_fixtures,
 )
 
 
 @pytest.mark.parametrize(
-    "attrs, field_value",
-    field_positive_validate,
+    "field_type, attrs, field_value",
+    fields_positive_fixtures,
 )
-def test_field_validate_positive(attrs, field_value):
-    field = Field(**attrs)
-    field.validate(field_value)
-
-
-@pytest.mark.parametrize("attrs, field_value, expected", field_negative_validate)
-def test_field_validate_negative(attrs, field_value, expected):
-    field = Field(**attrs)
-    with pytest.raises(ValueError) as e:
-        field.validate(field_value)
-    assert str(e.value) == expected.format(field_name=field.name)
-
-
-@pytest.mark.parametrize(
-    "attrs, field_value",
-    char_field_positive_validate,
-)
-def test_char_field_validate_positive(attrs, field_value):
-    field = CharField(**attrs)
-    field.validate(field_value)
-
-
-@pytest.mark.parametrize("attrs, field_value, expected", char_field_negative_validate)
-def test_char_field_validate_negative(attrs, field_value, expected):
-    field = CharField(**attrs)
-    with pytest.raises(ValueError) as e:
-        field.validate(field_value)
-    assert str(e.value) == expected.format(field_name=field.name)
-
-
-@pytest.mark.parametrize(
-    "attrs, field_value",
-    arguments_field_positive_validate,
-)
-def test_arguments_field_validate_positive(attrs, field_value):
-    field = ArgumentsField(**attrs)
+def test_field_validate_positive(field_type, attrs, field_value):
+    field = field_type(**attrs)
     field.validate(field_value)
 
 
 @pytest.mark.parametrize(
-    "attrs, field_value, expected", arguments_field_negative_validate
+    "field_type, attrs, field_value, expected",
+    fields_negative_fixtures,
 )
-def test_arguments_field_negative_validate(attrs, field_value, expected):
-    field = ArgumentsField(**attrs)
+def test_fields_validate_negative(field_type, attrs, field_value, expected):
+    field = field_type(**attrs)
     with pytest.raises(ValueError) as e:
         field.validate(field_value)
     assert str(e.value) == expected.format(field_name=field.name)
